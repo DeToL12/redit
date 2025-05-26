0966586e7ad607b2d709a47d2222122fa47dd2af
DeToL12 <103846849+DeToL12@users.noreply.github.com>
2025-05-26 16:59:19 +0300

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://update-sinfo.vercel.app"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://update-ssdsinfo.vercel.app"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
