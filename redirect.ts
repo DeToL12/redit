aedf3e66e8b8b5d8bf30e522d2cd3ea1cd5cf203
DeToL12 <103846849+DeToL12@users.noreply.github.com>
2025-06-23 19:25:08 +0300

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://update-ssdsinfo.vercel.app"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://icat.org.pe/greens/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
